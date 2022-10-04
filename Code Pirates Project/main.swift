f39038807a1d985ae3534aa7dc0463ae8ebeff08
Raghav <dhawan.raghav25@gmail.com>
2022-10-05 00:53:54 +0530

    Added Employee Class

--- Code Pirates Project/main.swift
+++ Code Pirates Project/main.swift
@@ -1,1 +1,32 @@
-print("Project Init")
+import Foundation
+
+class Employee {
+    var name: String! = nil
+    var birthYear: UInt!
+    var monthlySalary: UInt!
+    var rate: UInt! = 100{
+        didSet(newValue){
+            if newValue>100{
+                rate = 100
+            }
+            if newValue<10{
+                rate = 10
+            }
+        }
+    }
+    
+    //Computed Property
+    var age: UInt {
+        return UInt(Calendar.current.component(.year, from: Date()))-birthYear
+    }
+    
+    //Initialization
+    init(name: String!, birthYear: UInt!, monthlySalary: UInt!, rate: UInt!) {
+        self.name = name
+        self.birthYear = birthYear
+        self.monthlySalary = monthlySalary
+        self.rate = rate
+        
+    }
+    
+}
